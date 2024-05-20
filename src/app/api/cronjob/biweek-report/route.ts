51a00d6334a8bb32ca4644ddce49c3f8f1332247
Alexander Victoria <16567450+Dazpel@users.noreply.github.com>
2024-05-20 11:36:43 -0400

    test: testing github actions

---
+++ src/app/api/cronjob/biweek-report/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import prisma from "@lib/prisma/prismaClient";
+
+export async function GET(request: Request, response: Response) {
+  try {
+    if (
+      request.headers.get("Authorization") !==
+      `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return Response.json(
+        { message: "Invalid authorization header" },
+        { status: 200 }
+      );
+    }
+    console.log("-----------------------------------");
+    console.log("------- Request to create bi-weekly report -------");
+    console.log("-----------------------------------");
+
+    const reports = await prisma.report.findMany({
+      select: {
+        reportName: true,
+      },
+    });
+
+    console.log(reports);
+
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ status: 500 });
+  }
+}
